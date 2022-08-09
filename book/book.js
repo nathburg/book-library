8444e4c979a9fbc64e6984bf39574510711b9a7a
Nathan Burgess <nathans.email.address@gmail.com>
2022-08-09 12:43:57 -0700

    detail pages load

---
+++ book/book.js
@@ -0,0 +1,14 @@
+import { renderBookDetail } from "../render-utils.js";
+import { getBook } from "../fetch-utils.js";
+
+const bookContainerEl = document.querySelector('#book-detail-container');
+
+const params = new URLSearchParams(window.location.search);
+
+loadData();
+
+async function loadData() {
+    const book = await getBook(params.get('id'));
+    const bookDiv = renderBookDetail(book);
+    bookContainerEl.append(bookDiv);
+}
--- render-utils.js
+++ render-utils.js
@@ -11,3 +11,26 @@ export function renderBookThumbnail(book) {
 
     return bookEl;
 }
+
+export function renderBookDetail (book) {
+    const bookEl = document.createElement('div');
+    const bookTitleEl = document.createElement('h1');
+    const bookDetailsEl = document.createElement('p');
+
+    const genres = book.Genres;
+    let genreString = '';
+    for (let i = 0; i < genres.length; i++) {
+        if (i < genres.length - 1) {
+            genreString = genreString + ' ' + genres[i] + ',';
+        } else {
+            genreString = genreString + ' and ' + genres[i];
+        }
+    }
+
+    bookTitleEl.textContent = book.Title;
+    bookDetailsEl.textContent = `${book.Title} was written by ${book.Author}. It was published in ${book.Published} and is classified as ${genreString}.`;
+
+    bookEl.append(bookTitleEl, bookDetailsEl);
+
+    return bookEl;
+}
